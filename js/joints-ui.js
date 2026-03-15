d8b034f004bda316196f3a4599c4ca4e063c582d
baymin <xiaobai.cw@gmail.com>
2026-03-15 20:45:23 +0800

    Update joints-ui.js

--- js/joints-ui.js
+++ js/joints-ui.js
@@ -3,7 +3,6 @@ import {
   PRISMATIC_FALLBACK_RANGE,
   ROTARY_FALLBACK_RANGE,
 } from './config.js';
-import { clamp, cssSafe, formatJointValue } from './utils.js';
 
 export class JointsUI {
   constructor(container, countElement, callbacks) {
